bb0942bee40ea001385f26b5954b92e1a742d37d
okudako <longsleeper@gmail.com>
2018-04-01 14:18:53 +0900

    Update comaru_esp32.ino

--- comaru_esp32.ino
+++ comaru_esp32.ino
@@ -4,8 +4,8 @@
 //
 // Library : Arduino core for ESP32 WiFi chip
 //           https://github.com/espressif/arduino-esp32
-//           WiFiClientSecure Library(version 1.0.0)
-//           ArduinoJson Library (version 5.13.1)
+//           WiFiClientSecure Library (Version 1.0.0)
+//           ArduinoJson Library      (Version 5.13.1)
 //           IRremote Arduino Library (Version 2.2.3)
 
 #include <WiFiClientSecure.h>
@@ -59,245 +59,75 @@ int sendPin = 26;
 IRrecv irrecv(recvPin);
 IRsend irsend(sendPin);
 
+// Allocate JsonBuffer
+DynamicJsonBuffer jsonBuffer(4096);
+
 //+=============================================================================
 // Configure the Arduino
 //
 void  setup ( )
 {
-  // Setial Setting
-  Serial.begin(115200);   // Status message will be sent to PC at 115200 baud
-  delay(1000);
 
   // Wifi Flow
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
 
   // attempt to connect to Wifi network:
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    // wait 1 second for re-trying
     delay(1000);
   }
-  Serial.print("Connected to ");
-  Serial.println(ssid);
 
   // HTTPS connecting 
   client.setCACert(root_ca);
-
-  Serial.println("\nStarting connection to server...");
+  
   if (!client.connect(server, 443))
-    Serial.println("Connection failed!");
-  else {
-    Serial.println("Connected to server!");
-    // Make a HTTP request:
-    client.println("GET /api/v1/?ProductNo=RMF-TX200J&Command=PowerOn HTTP/1.1");
-    client.println("Host: comaru.herokuapp.com");
-    client.println("Connection: close");
-    client.println();
+  {
+    
+  } else {
+      // Make a HTTP request:
+      client.println("GET /api/v1/?ProductNo=RMF-TX200J&Command=PowerOn HTTP/1.1");
+      client.println("Host: comaru.herokuapp.com");
+      client.println("Connection: close");
+      client.println();
 
-  if (client.println() == 0) {
-    Serial.println(F("Failed to send request"));
-    return;
-  }
-
-    while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      Serial.print(line);
-      if (line == "\r") {
-        Serial.println("headers received");
-        break;
+      if (client.println() == 0) {
+        return;
       }
+
+      while (client.connected()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          break;
+        }
+      }
+
+    // Parse JSON object
+    String c = "";
+    while (client.available()) {
+        c.concat((char)client.read());  
     }
-
-  // Allocate JsonBuffer
-  // Use arduinojson.org/assistant to compute the capacity.
-  const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
-  DynamicJsonBuffer jsonBuffer(4096);
-
-  // Parse JSON object
-  String c = "";
-  while (client.available()) {
-    c.concat((char)client.read());  
+    JsonObject& root = jsonBuffer.parseObject(c);
+    if (!root.success()) {
+      return;
     }
-
-  // Magic code
-  c.remove(c.length()-1);
-  Serial.println(c);
-
-  // Parse JSON object
-  JsonObject& root = jsonBuffer.parseObject(c);
-  if (!root.success()) {
-    Serial.println(F("Parsing failed!"));
-    return;
-  }
-
-    // Extract values
-    Serial.println(F("Response:"));
-    Serial.println(root["Response"][0]["Manufacturer"].as<char*>());
-    Serial.println(root["Response"][0]["ProductName"].as<char*>());
-    Serial.println(root["Response"][0]["ProductNo"].as<char*>());
-    Serial.println(root["Response"][0]["Herz"].as<char*>());
-    Serial.println(root["Response"][0]["Command"].as<char*>());
-    Serial.println(root["Response"][0]["RawData"][0].as<char*>());
-
+    
+    // IR Signal set
     IR_Signal_Length = root["Response"][0]["RawData"].size();
     IR_ROW_DATA = new unsigned int [IR_Signal_Length];
     for (int i = 0; i < IR_Signal_Length; i++) {
       IR_ROW_DATA[i] = root["Response"][0]["RawData"][i].as<unsigned int>();
-      Serial.println(IR_ROW_DATA[i]);
-      Serial.println(IR_Signal_Length);
-      Serial.println(sizeof (root["Response"][0]["RawData"]));
     }
     IR_Herz     = root["Response"][0]["Herz"].as<int>();
 
+    // Client release
     client.stop();
   }
 
-
-  irrecv.enableIRIn();  // Start the receiver
-}
-
-//+=============================================================================
-// Display IR code
-//
-void  ircode (decode_results *results)
-{
-  // Panasonic has an Address
-  if (results->decode_type == PANASONIC) {
-    Serial.print(results->address, HEX);
-    Serial.print(":");
+  // Send IR Signal
+  for (int i = 0; i < 3; i++) {
+    irsend.sendRaw(IR_ROW_DATA, IR_Signal_Length, IR_Herz ); 
+    delay(40); 
   }
 
-  // Print Code
-  Serial.print(results->value, HEX);
-}
-
-//+=============================================================================
-// Display encoding type
-//
-void  encoding (decode_results *results)
-{
-  switch (results->decode_type) {
-    default:
-    case UNKNOWN:      Serial.print("UNKNOWN");       break ;
-    case NEC:          Serial.print("NEC");           break ;
-    case SONY:         Serial.print("SONY");          break ;
-    case RC5:          Serial.print("RC5");           break ;
-    case RC6:          Serial.print("RC6");           break ;
-    case DISH:         Serial.print("DISH");          break ;
-    case SHARP:        Serial.print("SHARP");         break ;
-    case JVC:          Serial.print("JVC");           break ;
-    case SANYO:        Serial.print("SANYO");         break ;
-    case MITSUBISHI:   Serial.print("MITSUBISHI");    break ;
-    case SAMSUNG:      Serial.print("SAMSUNG");       break ;
-    case LG:           Serial.print("LG");            break ;
-    case WHYNTER:      Serial.print("WHYNTER");       break ;
-    case AIWA_RC_T501: Serial.print("AIWA_RC_T501");  break ;
-    case PANASONIC:    Serial.print("PANASONIC");     break ;
-    case DENON:        Serial.print("Denon");         break ;
-  }
-}
-
-//+=============================================================================
-// Dump out the decode_results structure.
-//
-void  dumpInfo (decode_results *results)
-{
-  // Check if the buffer overflowed
-  if (results->overflow) {
-    Serial.println("IR code too long. Edit IRremoteInt.h and increase RAWBUF");
-    return;
-  }
-
-  // Show Encoding standard
-  Serial.print("Encoding  : ");
-  encoding(results);
-  Serial.println("");
-
-  // Show Code & length
-  Serial.print("Code      : ");
-  ircode(results);
-  Serial.print(" (");
-  Serial.print(results->bits, DEC);
-  Serial.println(" bits)");
-}
-
-//+=============================================================================
-// Dump out the decode_results structure.
-//
-void  dumpRaw (decode_results *results)
-{
-  // Print Raw data
-  Serial.print("Timing[");
-  Serial.print(results->rawlen-1, DEC);
-  Serial.println("]: ");
-
-  for (int i = 1;  i < results->rawlen;  i++) {
-    unsigned long  x = results->rawbuf[i] * USECPERTICK;
-    if (!(i & 1)) {  // even
-      Serial.print("-");
-      if (x < 1000)  Serial.print(" ") ;
-      if (x < 100)   Serial.print(" ") ;
-      Serial.print(x, DEC);
-    } else {  // odd
-      Serial.print("     ");
-      Serial.print("+");
-      if (x < 1000)  Serial.print(" ") ;
-      if (x < 100)   Serial.print(" ") ;
-      Serial.print(x, DEC);
-      if (i < results->rawlen-1) Serial.print(", "); //',' not needed for last one
-    }
-    if (!(i % 8))  Serial.println("");
-  }
-  Serial.println("");                    // Newline
-}
-
-//+=============================================================================
-// Dump out the decode_results structure.
-//
-void  dumpCode (decode_results *results)
-{
-  // Start declaration
-  Serial.print("unsigned int  ");          // variable type
-  Serial.print("rawData[");                // array name
-  Serial.print(results->rawlen - 1, DEC);  // array size
-  Serial.print("] = {");                   // Start declaration
-
-  // Dump data
-  for (int i = 1;  i < results->rawlen;  i++) {
-    Serial.print(results->rawbuf[i] * USECPERTICK, DEC);
-    if ( i < results->rawlen-1 ) Serial.print(","); // ',' not needed on last one
-    if (!(i & 1))  Serial.print(" ");
-  }
-
-  // End declaration
-  Serial.print("};");  // 
-
-  // Comment
-  Serial.print("  // ");
-  encoding(results);
-  Serial.print(" ");
-  ircode(results);
-
-  // Newline
-  Serial.println("");
-
-  // Now dump "known" codes
-  if (results->decode_type != UNKNOWN) {
-
-    // Some protocols have an address
-    if (results->decode_type == PANASONIC) {
-      Serial.print("unsigned int  addr = 0x");
-      Serial.print(results->address, HEX);
-      Serial.println(";");
-    }
-
-    // All protocols have data
-    Serial.print("unsigned int  data = 0x");
-    Serial.print(results->value, HEX);
-    Serial.println(";");
-  }
 }
 
 //+=============================================================================
@@ -305,20 +135,5 @@ void  dumpCode (decode_results *results)
 //
 void  loop ( )
 {
-  decode_results  results;        // Somewhere to store the results
-
-  if (irrecv.decode(&results)) {  // Grab an IR code
-    dumpInfo(&results);           // Output the results
-    dumpRaw(&results);            // Output the results in RAW format
-    dumpCode(&results);           // Output the results as source code
-    Serial.println("");          // Blank line between entries
-    irrecv.resume();              // Prepare for the next value
-  }
-
-  for (int i = 0; i < 3; i++) {
-  irsend.sendRaw(IR_ROW_DATA, IR_Signal_Length, IR_Herz ); 
-  delay(40); 
-  }
-  delay(3000);
-  
+  // No use
 }
